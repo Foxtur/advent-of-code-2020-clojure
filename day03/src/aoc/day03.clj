120a1bc538e40fb033b24b56bf63bd1db42eb1b0
Dominik Schenner <admin@foxtur.com>
2020-12-04 22:18:56 +0100

    Refactored solution day03 with usage of iterate/take-nth function

--- day03/src/aoc/day03.clj
+++ day03/src/aoc/day03.clj
@@ -7,19 +7,11 @@
       s/split-lines))
 
 (defn solve [input right down]
-  (let [rowcount (count input)]
-    (loop [col 0
-           row 0
-           collisions 0]
-      (if (< row rowcount)
-        (let [current-row (nth input row)
-              char (nth current-row col)
-              tree? (= \# char)]
-          (recur
-           (rem (+ col right) (count (nth input row)))
-           (+ row down)
-           (if tree? (inc collisions) collisions)))
-        collisions))))
+  (let [width (count (first input))
+        columns (map #(mod % width) (iterate #(+ % right) 0))
+        spots (map nth input (take-nth down columns))
+        trees (filter #(= \# %) spots)]
+    (count trees)))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -27,12 +19,15 @@
   (let [input (read-input "input01.txt")]
     (println "Solution Part 01:" (solve input 3 1))
     (println "Solution Part 02:"
-             (reduce *
-                     (map
-                      #(solve input (first %) (second %))
-                      [[1 1] [3 1] [5 1] [7 1] [1 2]])))))
+             (reduce * (map #(solve input (first %) (second %))
+                            [[1 1] [3 1] [5 1] [7 1] [1 2]])))))
 
 (comment
   (-main)
+  (let [input (read-input "input01.txt")]
+    (map
+     #(solve input (first %) (second %))
+     [[1 1] [3 1] [5 1] [7 1] [1 2]]))
+
   (solve (read-input "input01.txt") 3 1)
   (first (read-input "input01.txt")))
