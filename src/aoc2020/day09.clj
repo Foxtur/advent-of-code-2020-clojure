27be123b832113312182a5b3eb5d14e0f2afba94
Dominik Schenner <admin@foxtur.com>
2020-12-10 02:02:41 +0100

    code cleanup

--- src/aoc2020/day09.clj
+++ src/aoc2020/day09.clj
@@ -5,16 +5,14 @@
 (defn read-input []
   (->> (slurp (io/resource "input_day09.txt"))
        str/split-lines
-       (map #(Long/parseLong %))
-       vec))
+       (mapv #(Long/parseLong %))))
 
 (def actual-input (read-input))
 
 (def test-input
   (->> "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576\n"
        str/split-lines
-       (map #(Long/parseLong %))
-       vec))
+       (mapv #(Long/parseLong %))))
 
 (defn valid-number? [preamble n]
   (let [combinations (vec (filter #(not= (first %) (second %)) (for [x preamble y preamble] [x y])))
@@ -23,17 +21,17 @@
 
 (defn find-contiguous-set [input n]
   (loop [s 0]
-    (let [result (reduce (fn [a b] (let [sum (+ a b)]
+    (let [end-element (reduce (fn [a b] (let [sum (+ a b)]
                                      (cond
                                        (< sum n) sum
                                        (= sum n) (reduced b)
                                        :else (reduced false))))
                          (drop s input))]
-      (if result
-        (set (subvec input s (inc (.indexOf input result))))
+      (if end-element
+        (set (subvec input s (inc (.indexOf input end-element))))
         (recur (inc s))))))
 
-(defn walk-input [input preamble-size]
+(defn find-invalid-input [input preamble-size]
   (->> input
        (partition (inc preamble-size) 1)
        (filter #((complement valid-number?) % (last %)))
@@ -41,7 +39,7 @@
        last))
 
 (defn solve-part01 []
-  (walk-input actual-input 25))
+  (find-invalid-input actual-input 25))
 
 (defn solve-part02 []
   (let [contiguous-set (find-contiguous-set actual-input 104054607)
